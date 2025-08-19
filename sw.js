e04c830b111e575c7aa3bf9f14d5e5e731de6439
당신의이름 <you@example.com>
2025-08-19 21:05:31 +0900

    서비스 워커 추가

---
+++ sw.js
@@ -0,0 +1,33 @@
+// sw.js
+const CACHE_NAME = 'fastmate-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/fastmate.html',
+  '/login.html',
+  '/signup.html',
+  '/mypage.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
